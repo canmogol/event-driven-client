4562b510a7d96b7d50a82f3809bfd13744a0ade0
canmogol <canmogol@gmail.com>
2015-10-12 10:08:26 +0300

    client code generated by cxf's wadl2java, client using resteasy as client proxy

---
+++ src/main/java/com/lambstat/Client.java
@@ -0,0 +1,35 @@
+package com.lambstat;
+
+import com.lambstat.module.jetty.data.LoginResponse;
+import com.lambstat.module.jetty.resource.JettyResource;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+
+public class Client {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.doRSCall();
+    }
+
+    private void doRSCall() {
+        JettyResource resource = Resources.create(
+                JettyResource.class,
+                "http://localhost:8080/"
+        );
+        this.call(resource);
+    }
+
+    private void call(JettyResource resource) {
+        System.out.println("resource: " + resource);
+        // call with wrong credentials
+        LoginResponse loginResponse = resource.login("wrong", "credentials");
+        System.out.println(ToStringBuilder.reflectionToString(loginResponse));
+
+        // correct credentials
+        loginResponse = resource.login("john", "123");
+        System.out.println(ToStringBuilder.reflectionToString(loginResponse));
+
+    }
+
+}
---
+++ src/main/java/com/lambstat/Resources.java
@@ -0,0 +1,33 @@
+package com.lambstat;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+public class Resources<T> {
+
+    private static Resources instance = null;
+
+    private Resources() {
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T create(Class<T> t, String url) {
+        return (T) getInstance().createResource(t, url);
+    }
+
+    private T createResource(Class<T> t, String url) {
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        ResteasyWebTarget target = client.target(url);
+        return target.proxy(t);
+    }
+
+    private static Resources getInstance() {
+        if (instance == null) {
+            instance = new Resources();
+        }
+        return instance;
+    }
+
+}
+
